e0dc0eff5193603651dca7079f595db7fbde29cb
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-04-14 23:45:46 +0200

    add api integration

--- apps/webshop/src/pages/index.tsx
+++ apps/webshop/src/pages/index.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from 'react';
 import styles from './index.module.css';
 
+interface User {
+  id: string;
+  fullName: string;
+}
+
 export function Index() {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            user(id: 1) {
+              id
+              fullName
+            }
+          }
+        `,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        setUser(data.data.user);
+      });
+  }, []);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className={styles.page}>
-      <h1>Welcome to Kramp!</h1>
+      <h1>Welcome to Kramp, {user.fullName}!</h1>
     </div>
   );
 }
